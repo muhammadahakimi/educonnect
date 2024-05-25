58e118ca4d4281effa199085e6d00d9753e88f50
Muhammad Hakimi <163388531+muhammadahakimi@users.noreply.github.com>
2024-05-25 21:15:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,11 @@
   <script src="js/setup.js"></script>
   <style>
     body {
+      padding: 50px;
+    }
+    body > div {
       display: flex;
       gap: 10px;
-      padding: 50px;
     }
     button {
       display: block;
@@ -40,14 +42,16 @@
 </head>
 <body>
   <h1>EduConnect</h1>
-  <button onclick="page('logout')">Logout</button>
-  <button onclick="page('my_profile')">My Profile</button>
-  <button onclick="page('manage_user')">Manage User</button>
-  <button onclick="page('class')">Class</button>
-  <button onclick="page('result_exam')">Result Exam</button>
-  <button onclick="page('group')">Group</button>
-  <?php if ($user->role == "student") { print "<button onclick=\"page('my_homework')\">My Homework</button>"; } ?>
-  <button onclick="page('chat')">Chat</button>
+  <div>
+    <button onclick="page('logout')">Logout</button>
+    <button onclick="page('my_profile')">My Profile</button>
+    <button onclick="page('manage_user')">Manage User</button>
+    <button onclick="page('class')">Class</button>
+    <button onclick="page('result_exam')">Result Exam</button>
+    <button onclick="page('group')">Group</button>
+    <?php if ($user->role == "student") { print "<button onclick=\"page('my_homework')\">My Homework</button>"; } ?>
+    <button onclick="page('chat')">Chat</button>
+  </div>
   <img src="resources/images/background.jpg">
 </body>
 <script>
